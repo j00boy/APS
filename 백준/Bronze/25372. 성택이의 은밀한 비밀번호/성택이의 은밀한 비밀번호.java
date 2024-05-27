16a17990753a0b0f1481be0e00fd48514a237f7a
j00boy <kkjjhh9705@gmail.com>
2024-05-27 12:12:06 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 312 ms, Memory: 20448 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        for(int i = 0; i < N; i++) {
+            String str = sc.next();
+            
+            if(str.length() >= 6 && str.length() <= 9) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
