4be1ca6324f8cd9d14d72f0303620bceb03cce5a
j00boy <kkjjhh9705@gmail.com>
2024-05-27 12:10:05 +0900

    [Bronze V] Title: 연세대학교, Time: 196 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        if(n == 0) {
+            System.out.println("YONSEI");
+        } else {
+            System.out.println("Leading the Way to the Future");
+        }
+        
+        /*
+        연세대학교의 영문명은 YONSEI, 슬로건은 Leading the Way to the Future이다.
+
+이를 출력하는 프로그램을 작성해보도록 하자.
+        */
+    }
+}
