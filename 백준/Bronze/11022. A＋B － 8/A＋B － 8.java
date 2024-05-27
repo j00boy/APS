748db5166a0ddf718dea4e3c5a70b530d042f6c6
j00boy <kkjjhh9705@gmail.com>
2024-05-27 12:06:58 +0900

    [Bronze V] Title: A+B - 8, Time: 276 ms, Memory: 19304 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        for(int i = 1; i <= N; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+        
+            int C = A + B;
+            
+            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + C);
+        }
+        
+    }
+}
