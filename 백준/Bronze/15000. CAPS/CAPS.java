096d88eef927777166b374a7a026e484950bb66b
j00boy <kkjjhh9705@gmail.com>
2024-05-30 08:43:38 +0900

    [Bronze V] Title: CAPS, Time: 428 ms, Memory: 27852 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.next();
+        String ans = "";
+        
+        ans = str.toUpperCase();
+                
+        System.out.println(ans);
+    }
+}
