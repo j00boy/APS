0e623aac87230d1bb3d8c4b92fa6018f54faa9d8
j00boy <kkjjhh9705@gmail.com>
2024-06-28 08:42:52 +0900

    [Bronze IV] Title: 히스토그램, Time: 256 ms, Memory: 18472 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        
+        for(int i = 0; i < a; i++) {
+            int b = sc.nextInt();
+            
+            String str = "";
+            for(int j = 0; j < b; j++) {
+                str += '=';
+            }
+            
+            System.out.println(str);
+        }
+    }
+}
