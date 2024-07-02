aadc7b3bf0814c2a11c9761a4b7814c0ece5d42e
j00boy <kkjjhh9705@gmail.com>
2024-07-02 23:55:43 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 512 ms, Memory: 39044 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	static int n;
+	static int[][] dots;
+	static Stack<Integer> stack;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		dots = new int[n][2];
+		stack = new Stack<>();
+		
+		int count = 0;
+		
+		for(int i = 0; i < n; i++) {
+			dots[i][0] = sc.nextInt();
+			dots[i][1] = sc.nextInt();
+		}
+		
+		for(int i = 0; i < n; i++) {
+			if(stack.isEmpty()) {	// 비어 있다면 무조건 넣기
+				stack.add(dots[i][1]);
+			} else {	// 안 비어 있다면
+				// 1. 그냥 더 높은 애가 나오면 stack에 축자
+				
+				// 2. 아니라면
+				// 새로운 스카이라인고도가 더 높아질때까지 count++
+				while(!stack.isEmpty() && stack.peek() > dots[i][1]) {
+					count++;
+					stack.pop();
+				}
+				
+				// 같은 높이는 이미 stack에 있으니까 추가 안됨
+				
+				// stack이 비어있거나, 높이가 다르다 == 새로운 스카이라인 고도가 더 높다
+				if(stack.isEmpty() || (stack.peek() != dots[i][1] && stack.peek() < dots[i][1])) {
+					stack.add(dots[i][1]);
+				}
+			}
+		}
+		
+//		System.out.println(count + ", " + stack);
+		
+		while(!stack.isEmpty()) {
+			int num = stack.pop();
+			if(num != 0) {	// 높이가 0인 빌딩은 있을 수가 없음
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+	}
+}
