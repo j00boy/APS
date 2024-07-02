400853e982bd0eea38d2e2dd868b67e1860200ca
j00boy <kkjjhh9705@gmail.com>
2024-07-02 23:59:03 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 508 ms, Memory: 38400 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -12,7 +12,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		n = sc.nextInt();
-		dots = new int[n][2];
+		dots = new int[n][2];	// x, y
 		stack = new Stack<>();
 		
 		int count = 0;
@@ -22,6 +22,7 @@ public class Main {
 			dots[i][1] = sc.nextInt();
 		}
 		
+		// x축은 안중요하고 y축이 높이니까 중요
 		for(int i = 0; i < n; i++) {
 			if(stack.isEmpty()) {	// 비어 있다면 무조건 넣기
 				stack.add(dots[i][1]);
@@ -31,6 +32,7 @@ public class Main {
 				// 2. 아니라면
 				// 새로운 스카이라인고도가 더 높아질때까지 count++
 				while(!stack.isEmpty() && stack.peek() > dots[i][1]) {
+//					System.out.println(stack);
 					count++;
 					stack.pop();
 				}
@@ -38,6 +40,10 @@ public class Main {
 				// 같은 높이는 이미 stack에 있으니까 추가 안됨
 				
 				// stack이 비어있거나, 높이가 다르다 == 새로운 스카이라인 고도가 더 높다
+//				if(!stack.isEmpty()) {
+//					System.out.println("스택: " + stack.peek() + ", 고도: " + dots[i][1]);					
+//				}
+				
 				if(stack.isEmpty() || (stack.peek() != dots[i][1] && stack.peek() < dots[i][1])) {
 					stack.add(dots[i][1]);
 				}
