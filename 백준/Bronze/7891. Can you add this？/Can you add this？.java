a2b9bc6f7893028e544e17af9bac001f8e3cdf10
j00boy <kkjjhh9705@gmail.com>
2024-07-11 17:42:50 +0900

    [Bronze V] Title: Can you add this?, Time: 184 ms, Memory: 17432 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++) {
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            
+            System.out.println(num1 + num2);
+        }
+    }
+}
