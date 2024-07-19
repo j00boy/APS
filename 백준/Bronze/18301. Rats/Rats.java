ac5dc57def837947042470f2572eae00a7d640dd
j00boy <kkjjhh9705@gmail.com>
2024-07-19 23:37:33 +0900

    [Bronze V] Title: Rats, Time: 208 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // (n1 + 1)(n2 + 1)/(n12 + 1) - 1
+        
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n12 = sc.nextInt();
+        
+        System.out.println((n1 + 1) * (n2 + 1) / (n12 + 1) - 1);
+    }
+}
