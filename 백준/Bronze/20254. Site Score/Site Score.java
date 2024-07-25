9bf3bf297f3cd69d44bdc391d638f0cada8ad5cf
j00boy <kkjjhh9705@gmail.com>
2024-07-26 08:40:32 +0900

    [Bronze V] Title: Site Score, Time: 208 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,15 @@
+// 56UR + 24TR + 14UO + 6TO
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        
+        System.out.println(56 * a + 24 * b + 14 * c + 6 * d);
+    }
+}
